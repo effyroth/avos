97e7a41d42010c8cad7542da3ea62ce65ba78d98
guzhen <zhengu@sohu-inc.com>
2015-03-31 15:01:51 +0800

    a

--- main/com/avos/avoscloud/AVOnlineConfigureListener.java
+++
@@ -1,8 +0,0 @@
-package com.avos.avoscloud;
-
-import org.json.JSONObject;
-
-public abstract interface AVOnlineConfigureListener
-{
-  public abstract void onDataReceived(JSONObject paramJSONObject);
-}
